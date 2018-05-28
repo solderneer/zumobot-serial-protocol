389ca0d3422994238a5aeaf4b36fdfcb4aef6595
Sudharshan <sudhar393@gmail.com>
2018-05-28 09:33:59 +0800

    basic init script

--- inc/settings.h
+++ inc/settings.h
@@ -1,1 +1,7 @@
-#define F_CPU 8000000UL
+#ifndef __SETTINGS_H
+#define __SETTINGS_H
+
+#define F_CPU 16000000UL
+#define BAUD 115200
+
+#endif /* __SETTINGS_H */
--- src/Zumo32U4Interface.cpp
+++ src/Zumo32U4Interface.cpp
@@ -1,1 +1,20 @@
-#include 
+#include <avr/io.h>
+#include <Zumo32U4Interface.h>
+#include <util/setbaud.h>
+#include <settings.h>
+
+void Zumo32U4Interface::init2()
+{
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+
+#if USE_2X
+    UCSR0A |= (1 << U2X0);
+#else
+    UCSR0A &= (1 << U2X0);
+#endif
+
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+
+}
