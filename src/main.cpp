d3a6375849d913cff07625366c2d88b24a4b96ce
Sudharshan <sudhar393@gmail.com>
2018-06-09 07:43:11 +0800

    the serial class is not functional

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,8 @@
 int main(void)
 {
 	for (;;) {
-        Zumo32U4Motors::setSpeeds(100, 100);
         Zumo32U4Serial::UART_TransmitByte('c');
 		_delay_ms(1000);
-        Zumo32U4Motors::setSpeeds(-100, -100);
-        _delay_ms(1000);
 	}
 
 	return -1;
