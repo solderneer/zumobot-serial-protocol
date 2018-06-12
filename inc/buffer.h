844655f0153638e8f7d7b5b62f8117a387063300
Sudharshan <sudhar393@gmail.com>
2018-06-12 15:43:38 +0800

    added buffer class implementation

--- inc/buffer.h
+++ inc/buffer.h
@@ -3,11 +3,15 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef enum {FAIL, SUCCESS} status_t;
+
 class Buffer {
 public:
     Buffer(uint16_t buff_size);
-    int bufferReset(void);
-    int bufferPut(uint8_t byte);
+    ~Buffer();
+
+    void bufferReset(void);
+    void bufferPut(uint8_t byte);
     int bufferGet(uint8_t* byte);
     bool bufferEmpty(void);
     bool bufferFull(void);
@@ -15,4 +19,5 @@ private:
     uint8_t* buffer;
     size_t readPtr;
     size_t writePtr;
+    size_t buff_size;
 };
